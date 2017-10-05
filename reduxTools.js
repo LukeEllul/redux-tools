80c83d6e58d96e678825b9cf25a40ebaa278cd66
LukeEllul <ellul.luke@gmail.com>
2017-10-06 01:11:06 +0200

    added depthOf function to calculate depth of object

--- reduxTools.js
+++ reduxTools.js
@@ -2,24 +2,39 @@ const { createStore } = require('redux');
 const { combineReducers } = require('redux-immutable');
 const { Map } = require('immutable');
 
-function createReducer(initialState, handlers){
-    return function reducer(state = initialState, action){
-        const handle = typeof handlers === 'function' ? 
+function createReducer(initialState, handlers) {
+    return function reducer(state = initialState, action) {
+        const handle = typeof handlers === 'function' ?
             handlers(state, action) : handlers;
-        if(handle.hasOwnProperty(action.type))
+        if (handle.hasOwnProperty(action.type))
             return handle[action.type](state, action);
         else return state;
     }
 }
-
-const actionToMap = store => next => action => (next(action.toObject()), action);
 
 //how to use createReducer function:
 // const hey = createReducer({paws: 'nuccu'}, (state, action) => ({
 //     ['boccu']: () => Object.assign({}, state, {boccu: 'succu'})
 // }));
 
+const actionToMap = store => next => action => (next(action.toObject()), action);
+
+function depthOf(object) {
+    var level = 1;
+    var key;
+    for (key in object) {
+        if (!object.hasOwnProperty(key)) continue;
+
+        if (typeof object[key] == 'object') {
+            var depth = depthOf(object[key]) + 1;
+            level = Math.max(depth, level);
+        }
+    }
+    return level;
+}
+
 module.exports = {
     createReducer,
-    actionToMap
+    actionToMap,
+    depthOf
 };
