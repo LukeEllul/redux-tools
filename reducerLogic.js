be5134cff34645fa661c0e00062b29ce3439eadd
LukeEllul <ellul.luke@gmail.com>
2017-10-27 02:41:20 +0200

    created immutable, side-effect free addReducers functionality...still not 100% tested

--- reducerLogic.js
+++ reducerLogic.js
@@ -2,10 +2,11 @@ const { Map, List } = require('immutable');
 const R = require('ramda');
 const { deleteAll, createReducer } = require('./reduxTools');
 const { nest, toList } = require('./datastructures/dataStructures');
+const {createStore, applyMiddleware} = require('redux');
 
 const point = '.';
 
-const civilizedReducer = (reducer, name, intialState) =>
+const civilizedReducer = (name, reducer, intialState) =>
     (state = intialState || Map({}), action) => {
         if (state === 'getName') return name;
         return reducer(state, action);
@@ -20,12 +21,13 @@ const createUpperReducer = name => lowerReducer => {
             return (action.getState(state.get(name)), state);
         const v = action.type;
         const portion = v.slice(0, name.length);
+        const lowerReducerValue = lowerReducer(
+            state.get(name),
+            nest(action)('type', v.slice(v.indexOf(point) + 1)).toJS()
+        );
+        if(typeof lowerReducerValue === 'function') return lowerReducerValue;
         return portion === name ?
-            state.set(name,
-                lowerReducer(
-                    state.get(name),
-                    nest(action)('type', v.slice(v.indexOf(point) + 1)).toJS()
-                )) : state;
+            state.set(name, lowerReducerValue) : state;
     }
 }
 
@@ -35,7 +37,8 @@ const changeLowerReducer = upperReducer => lowerReducer =>
 function makeSet(set = Map({})) {
     return (state = Map({}), action) => {
         if (state === 'getSet') return set;
-        const v = action.type;
+        const v = action && action.type;
+        if(!v) return state;
         const reducer = set.find(
             (val, k) => v === k || v.slice(0, v.indexOf(point)) === k);
         return reducer ? reducer(state, action) : state;
@@ -54,15 +57,30 @@ const addReducersToSet = (...reducers) => (set = makeSet()) =>
 
 const removeReducerFromSet = set => name => makeSet(set('getSet').delete(name));
 
+// const createSetOfReducers = name => (setOfReducers = makeSet()) => {
+//     let set = setOfReducers;
+//     let fn = createUpperReducer(name)(set);
+//     return (state = Map({}), action) => {
+//         if(action && action.reducers && action.type.slice(0, name.length) === name){
+//             set = addReducersToSet(...action.addReducers)(set);
+//             fn = createUpperReducer(name)(set);
+//         }
+//         return fn(state, action);
+//     }
+// }
+
 const createSetOfReducers = name => (setOfReducers = makeSet()) => {
-    let set = setOfReducers;
-    let fn = createUpperReducer(name)(set);
+    const fn = createUpperReducer(name)(setOfReducers);
     return (state = Map({}), action) => {
-        if(action && action.type.slice(0, name.length) === name && action.addReducers){
-            set = addReducersToSet(...action.addReducers)(set);
-            fn = createUpperReducer(name)(set);
+        if(state === 'getLowerReducer') return fn(state, action);
+        if(action && action.addReducers && action.type === name) {
+            return createSetOfReducers(name)(addReducersToSet(...action.addReducers)(setOfReducers));
         }
-        return fn(state, action);
+        const returnedValue = fn(state, action);
+        if(typeof returnedValue === 'function') {
+            return createSetOfReducers(name)(addReducersToSet(returnedValue)(setOfReducers));
+        }
+        return returnedValue;
     }
 }
 
@@ -73,6 +91,57 @@ const addToSetOfReducers = (...reducers) => setOfReducers =>
 
 const removeFromSetOfReducers = name => setOfReducers =>
     createUpperReducer(setOfReducers('getName'))(removeReducerFromSet(setOfReducers('getLowerReducer'))(name));
+
+
+
+const reds = [
+    createSetOfReducers('red1')()
+];
+
+const root = addToSetOfReducers(...reds)(createSetOfReducers('root')());
+
+const addReducersMiddleWare = store => next => action => {
+    const state1 = store.getState();
+    next(action);
+    if(action.addReducers){
+        const reducer = store.getState();
+        return (state, action) => reducer(typeof state === 'function' ? state1 : state, action);
+    }
+}
+
+const store = createStore(root, applyMiddleware(addReducersMiddleWare));
+
+store.dispatch({
+    type: 'root'
+});
+
+console.log(store.getState());
+
+const heyFn = (state = Map({}), action) => state.set('boccu', 'paws');
+
+const fn = store.dispatch({
+    type: 'root.red1',
+    addReducers: [addToSetOfReducers(createUpperReducer('red3')(heyFn))(createSetOfReducers('red4')())]
+});
+
+store.replaceReducer(fn);
+
+store.dispatch({
+    type: 'root.red1.red4.red3'
+});
+
+console.log(store.getState());
+
+store.replaceReducer(store.dispatch({
+    type: 'root.red1.red4',
+    addReducers: [createUpperReducer('red5')(heyFn)]
+}));
+
+store.dispatch({
+    type: 'root.red1.red4.red5'
+});
+
+console.log(store.getState());
 
 module.exports = {
     createUpperReducer,
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-merge
+const createSetOfReducers = name => (setOfReducers = makeSet()) => {
+    let set = setOfReducers;
+    let fn = createUpperReducer(name)(set);
+    return (state = Map({}), action) => {
+        if(action && action.reducers && action.type.slice(0, name.length) === name){
+            set = addReducersToSet(...action.addReducers)(set);
+            fn = createUpperReducer(name)(set);
+        }
+        return fn(state, action);
+    }
+}
