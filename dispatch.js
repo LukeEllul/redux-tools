a39092e7db27ba02e2301bd85c4f22db4f91b3f3
LukeEllul <ellul.luke@gmail.com>
2017-10-28 16:40:52 +0200

    dispatch.js tested and works :)

--- dispatch.js
+++ dispatch.js
@@ -1,7 +1,7 @@
 const { Map, List } = require('immutable');
 const R = require('ramda');
 const {createStore, applyMiddleware} = require('redux');
-const {createSetOfReducers, createUpperReducer, createSet} = require('./reducerLogic');
+const {createSetOfReducers, createUpperReducer, createSet, addReducersMiddleWare} = require('./reducerLogic');
 const {toMap} = require('./datastructures/dataStructures');
 
 const handleAddReducers = (returnedDispatch, prevAction = {}) => store => 
@@ -21,19 +21,3 @@ const dispatch = action => ([store, prevAction]) => {
 module.exports = {
     dispatch
 }
-
-const root = createSet(
-    createUpperReducer('red1')((state = Map({}), action) => state.set('boccu', 'paws')),
-    createSet(
-        createUpperReducer('1')((state = Map({}), action) => state.set('1', 'nuccu')),
-        createUpperReducer('2')((state = Map({}), action) => state.set('2', 'succu'))
-    )('red2')
-)('root');
-
-const store = createStore(root);
-
-store.dispatch({
-    type: 'root.red1'
-});
-
-console.log(store.getState());
