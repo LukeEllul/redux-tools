4d75bfc2e2746a5817f2d478a515ffa677cb3436
Luke Ellul <ellul.luke@gmail.com>
2017-10-29 20:48:16 +0100

    added showState and apply functions to dispatch.js

--- dispatch.js
+++ dispatch.js
@@ -2,22 +2,30 @@ const { Map, List } = require('immutable');
 const R = require('ramda');
 const {createStore, applyMiddleware} = require('redux');
 const {createSetOfReducers, createUpperReducer, createSet, addReducersMiddleWare} = require('./reducerLogic');
-const {toMap} = require('./datastructures/dataStructures');
+const {toMap, fromMap} = require('./datastructures/dataStructures');
 
 const handleAddReducers = (returnedDispatch, prevAction = {}) => store => 
     typeof returnedDispatch === 'function' ? (
         store.replaceReducer(returnedDispatch),
-        store.dispatch(prevAction),
+        store.dispatch(fromMap(prevAction)),
         store
     ) : store;
 
 const dispatch = action => ([store, prevAction]) => {
-    const returnedDispatch = store.dispatch(action);
+    const returnedDispatch = store.dispatch(fromMap(action));
     return [R.pipe(
         handleAddReducers(returnedDispatch, prevAction)
     )(store), action];
 }
 
+const showState = ([store, prevAction]) =>
+    (console.log(store.getState()), [store, prevAction]);
+
+const apply = (...actions) => store =>
+    R.pipe(...actions.map(action => dispatch(toMap(action))))([store]);
+
 module.exports = {
-    dispatch
+    dispatch,
+    apply,
+    showState
 }
